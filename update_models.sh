c4b36be7a25dd85cfe7031a3abf673a92411e050
Ryan Knell <ryan.knell@gmail.com>
2025-01-07 13:31:17 +1000

    chore: remove deprecated update_models.sh script containing MongoDB URI and export commands

--- update_models.sh
+++
@@ -1,4 +0,0 @@
-export MONGODB_URI='mongodb+srv://snappyapps:REMOVED_CREDENTIAL@cluster0.qzd6a.mongodb.net/distilleryPOS?retryWrites=true&w=majority'
-dart run bin/export_models.dart
-export MONGODB_URI='mongodb+srv://snappyapps:REMOVED_CREDENTIAL@cluster0.qzd6a.mongodb.net/distillerymanager?retryWrites=true&w=majority'
-dart run bin/export_models.dart
